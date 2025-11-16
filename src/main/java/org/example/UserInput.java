9e3ff5d38af403d6c79eea6f4175a3d9b1334846
Pareto <matix195@gmail.com>
2025-11-16 20:34:25 +0100

    UserInput before check repeated numbers

--- src/main/java/org/example/UserInput.java
+++ src/main/java/org/example/UserInput.java
@@ -11,26 +11,29 @@ public class UserInput {
     private Scanner sc = new Scanner(System.in);
     private ArrayList<Integer> userNumbers = new ArrayList<>();
 
-    public ArrayList<Integer> getNumbersFromUser(){
+    public ArrayList<Integer> getNumbersFromUser() {
         System.out.println("Welcome to Lotto! Please type 6 numbers and try your luck!");
-        try {
-            for (int i = 0; i < 6; i++) {
+        for (int i = 0; i < 6; i++) {
             System.out.print("Please type a number from 1 to 99: ");
-            int selectedNumber = sc.nextInt();
-            if (selectedNumber >= 1 && selectedNumber <= 99)
-                userNumbers.add(selectedNumber);
-            else {
-                System.out.println("The number is not in range. Try again!");
+            try {
+                int selectedNumber = sc.nextInt();
+                if (selectedNumber >= 1 && selectedNumber <= 99)
+                    userNumbers.add(selectedNumber);
+                else {
+                    System.out.println("The number is not in range. Try again!");
+                    i--;
+                }
+
+            } catch (InputMismatchException e) {
+                System.out.println("You entered text, please enter a number!");
+                sc.nextLine();
                 i--;
             }
         }
-        } catch (InputMismatchException e){
-            System.out.println("You entered text, please enter a number!");
-        }
-       return userNumbers;
+        return userNumbers;
     }
 
-    public void printUserNumbers(){
+    public void printUserNumbers() {
         System.out.println(userNumbers);
     }
 }
