f9d83f0b51524e7ae54572b15046dfb1f0220bc1
Pareto <matix195@gmail.com>
2025-11-18 20:09:26 +0100

    UserInputTest - first test

---
+++ src/test/java/org/example/UserInputTest.java
@@ -0,0 +1,30 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.Scanner;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+
+class UserInputTest {
+
+    @Test
+    void shouldGenerateSixNumbers(){
+        Scanner mockScanner = mock(Scanner.class);
+
+        when(mockScanner.nextInt())
+                .thenReturn(10, 20, 30, 35, 40, 50);
+
+        UserInput userInput = new UserInput(mockScanner);
+        List<Integer> numbers = userInput.getNumbersFromUser();
+
+        assertEquals(6, numbers.size());
+        assertTrue(numbers.contains(10));
+        assertTrue(numbers.contains(30));
+        assertTrue(numbers.contains(50));
+    }
+
+}
