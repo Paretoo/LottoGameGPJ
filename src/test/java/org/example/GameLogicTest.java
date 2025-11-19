01914cf67e5db0d3553ee9025e5949a01c89f8ab
Pareto <matix195@gmail.com>
2025-11-19 22:23:10 +0100

    GameLogicTest added

---
+++ src/test/java/org/example/GameLogicTest.java
@@ -0,0 +1,28 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GameLogicTest {
+    @Test
+    void shouldWinAPrize(){
+        ArrayList<Integer> enteredNumbers = new ArrayList<>(Arrays.asList(5, 10, 15, 20, 25, 30));
+        ArrayList<Integer> drawnNumbers = new ArrayList<>(Arrays.asList(5, 10 ,15, 20, 40, 50));
+
+        GameLogic game = new GameLogic(enteredNumbers, drawnNumbers);
+
+        Set<Integer> correctedNumbers = new HashSet<>(enteredNumbers);
+        correctedNumbers.retainAll(drawnNumbers);
+
+        assertEquals(4, correctedNumbers.size());
+        assertTrue(correctedNumbers.contains(10));
+        assertTrue(correctedNumbers.contains(20));
+    }
+
+}
